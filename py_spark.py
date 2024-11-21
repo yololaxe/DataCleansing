448d26f807f3b451b32515f228514761e32395a7
yololaxe <yololaxe971@gmail.com>
2024-11-21 12:32:29 +0100

    Clean réussi avec quelque commande et optimisation

--- py_spark.py
+++ py_spark.py
@@ -3,28 +3,32 @@ from pyspark.sql import SparkSession
 # Initialiser une session Spark
 spark = SparkSession.builder \
     .appName("OpenFoodFacts Data Cleansing") \
+    .config("spark.executor.memory", "4g") \
+    .config("spark.driver.memory", "4g") \
+    .config("spark.local.dir", "E:\EPSI\DataCleansing\spark-temp") \
     .config("spark.mongodb.input.uri", "mongodb://localhost:27017/openfoodfacts.products") \
     .getOrCreate()
 
+
 # Charger les données depuis un fichier CSV
-data = spark.read.csv("CSV\openfoodfacts.csv", header=True, inferSchema=True)
+data = spark.read.csv("CSV\openfoodfacts.csv", header=True, inferSchema=True, sep="\t")
+
+data = data.limit(1000)  # Charger uniquement 1000 lignes pour les tests
+
+# Remplacer les espaces et caractères spéciaux dans les noms de colonnes
+# Nettoyer les noms de colonnes
+data = data.toDF(*[c.strip().replace(" ", "_").replace(".", "_") for c in data.columns])
+
+# Vérifiez les colonnes disponibles
+print("Colonnes disponibles :", data.columns)
+
+# Suppression des lignes avec des valeurs manquantes dans certaines colonnes
+cleaned_data = data.dropna(subset=["product_name", "categories"])
+
+# Suppression des doublons basés sur 'code'
+cleaned_data = cleaned_data.dropDuplicates(subset=["code"])
+
+#cleaned_data.write.csv("CSV\cleaned_data.csv", header=True)
 
 # Afficher un échantillon
-data.show(5)
-
-# Afficher le schéma
-data.printSchema()
-
-# Statistiques descriptives
-data.describe().show()
-
-# Nombre de doublons
-print(f"Nombre de doublons : {data.count() - data.distinct().count()}")
-
-# Proportion de valeurs nulles par colonne
-from pyspark.sql.functions import col, sum as spark_sum
-
-null_counts = data.select([
-    spark_sum(col(c).isNull().cast("int")).alias(c) for c in data.columns
-])
-null_counts.show()
+cleaned_data.show(10)
