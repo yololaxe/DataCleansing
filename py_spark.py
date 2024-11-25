e951a27721959f3fd8d483405d3b90be381a8afa
yololaxe <yololaxe971@gmail.com>
2024-11-25 16:08:45 +0100

    Ajouter les écritures en JSON (pour plus de visibilité) + du profilage + More nettoyage des données

--- py_spark.py
+++ py_spark.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, trim
+import json
 
 # Initialiser une session Spark
 spark = SparkSession.builder \
@@ -13,7 +15,12 @@ spark = SparkSession.builder \
 # Charger les données depuis un fichier CSV
 data = spark.read.csv("CSV\openfoodfacts.csv", header=True, inferSchema=True, sep="\t")
 
-data = data.limit(1000)  # Charger uniquement 1000 lignes pour les tests
+data = data.limit(10000)  # Charger uniquement 1000 lignes pour les tests
+
+raw_json = data.toJSON().collect()
+
+with open("data.json", "w", encoding="utf-8") as json_file:
+    json.dump(raw_json, json_file, indent=4, ensure_ascii=False)
 
 # Remplacer les espaces et caractères spéciaux dans les noms de colonnes
 # Nettoyer les noms de colonnes
@@ -23,12 +30,44 @@ data = data.toDF(*[c.strip().replace(" ", "_").replace(".", "_") for c in data.c
 print("Colonnes disponibles :", data.columns)
 
 # Suppression des lignes avec des valeurs manquantes dans certaines colonnes
-cleaned_data = data.dropna(subset=["product_name", "categories"])
+
+important_columns = ["product_name", "categories", "brands"]
+# Supprimer les lignes où une ou plusieurs colonnes importantes sont nulles
+cleaned_data = data.dropna(subset=important_columns)
 
 # Suppression des doublons basés sur 'code'
 cleaned_data = cleaned_data.dropDuplicates(subset=["code"])
 
-#cleaned_data.write.csv("CSV\cleaned_data.csv", header=True)
+filtered_data = cleaned_data.filter(
+    (col("product_name").isNotNull()) &               # Vérifie que le nom n'est pas NULL
+    (trim(col("product_name")) != "") &              # Vérifie que le nom n'est pas vide
+    (~col("states").like("%discontinued%"))          # Exclut les produits marqués comme "discontinued"
+)
+
+simplified_data = filtered_data.select(
+    "code", "product_name", "brands", "categories", "countries"
+).limit(150)
+
+
+simplified_data.show(truncate=False)
+
+# Afficher le résultat
+# filtered_data.show()
 
 # Afficher un échantillon
-cleaned_data.show(10)
+# cleaned_data.show(10)
+
+json_data = simplified_data.toJSON().collect()
+
+with open("simplified_data.json", "w", encoding="utf-8") as json_file:
+    json.dump(json_data, json_file, indent=4, ensure_ascii=False)
+
+# Résumé final
+print("=== Résumé final ===")
+print(f"Nombre de lignes initiales : {data.count()}")
+print(f"Nombre de lignes après nettoyage : {cleaned_data.count()}")
+print(f"Nombre de lignes après filtrage : {filtered_data.count()}")
+print(f"Nombre de lignes exportées dans 'simplified_data.json' : {simplified_data.count()}")
+
+# Arrêter la session Spark
+spark.stop()
